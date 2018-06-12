7981643ab5a42cb20a7c17d7dcdbd9020a1a0ff5
PotatoeTrainYT <officialseb.12@gmail.com>
2018-06-12 22:01:57 +0100

    First release

---
+++ src/Swimming/Main.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Swimming;
+
+use pocketmine\block\Water;
+use pocketmine\entity\Entity;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
+use pocketmine\network\mcpe\protocol\PlayerActionPacket;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase implements Listener{
+
+    public function onEnable(): void{
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onMove(PlayerMoveEvent $event): void{
+        $player = $event->getPlayer();
+        if($player->getLevel()->getBlockAt($player->getFloorX(), $player->getFloorY(), $player->getFloorZ()) instanceof Water){
+            if(!$player->getGenericFlag($player::DATA_FLAG_SWIMMING)){
+                $player->setGenericFlag($player::DATA_FLAG_SWIMMING, true);
+                $eid = Entity::$entityCount++;
+                $pk = new PlayerActionPacket();
+                $pk->entityRuntimeId = $eid;
+                $pk->putBlockPosition($player->getFloorX(), $player->getFloorY(), $player->getFloorZ());
+                $pk->action = $pk::ACTION_START_SWIMMING;
+                $this->getServer()->broadcastPacket($this->getServer()->getOnlinePlayers(), $pk);
+            }else{
+                $player->setGenericFlag($player::DATA_FLAG_SWIMMING, false);
+                $eid = Entity::$entityCount++;
+                $pk = new PlayerActionPacket();
+                $pk->entityRuntimeId = $eid;
+                $pk->putBlockPosition($player->getFloorX(), $player->getFloorY(), $player->getFloorZ());
+                $pk->action = $pk::ACTION_STOP_SWIMMING;
+                $this->getServer()->broadcastPacket($this->getServer()->getOnlinePlayers(), $pk);
+                $player->setGenericFlag($player::DATA_FLAG_SWIMMING, false);
+            }
+        }
+    }
+}
